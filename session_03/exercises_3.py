3967b275f31b0b05e7a6b4ad416b7eb284869b04
Replit user <>
2023-07-17 13:33:56 +0000

    session 4

--- session_03/exercises_3.py
+++ session_03/exercises_3.py
@@ -3,37 +3,69 @@
 ## Section A
 # 1. Ask for the user's name, if they are called "Bob", print "Welcome Bob!".
 
+'''name = input("What is your name? ")
 
+if name == 'Bob':
+ print("Welcome " + name + "!") '''
 
 # 2. Ask for the user's name, if they are not called "Alice", print "You're not Alice!".
-
+'''
+name2 = input("What is your name? ")
+if name2 != "Alice":
+  print("Your name is not Alice!") '''
 
 
 # 3. Ask the user for a password, if they enter the password "qwerty123", print "You have successfully logged in". 
-#   If they get it wrong, print "Password failure".
-
+'''  If they get it wrong, print "Password failure".
+password = input("What is your password? ")
+if password == "qwerty123":
+  print("You have successfuly logged in")
+else: print("Password Failure!") '''
 
 
 # 4. Ask the user to enter a number, if the number is even, print "Even", otherwise print "Odd".
-
+'''number = int(input("Enter a number: "))
+if number % 2 == 0:
+   print("Even")
+else: print ("Odd") '''
 
 
 # 5. Ask the user for 2 different numbers, if the total of the two numbers is over 21, print "Bust" otherwise print "Safe"
+'''
+number2 = int(input("Enter a number: "))
+number3 = int(input("Enter a number: "))
 
-
+if number2 & number3 >= 21:
+  print("Bust")
+else: print("Safe") '''
 
 # 6. Ask the user to enter the length and width of a shape and check if it is a square or not.
 
+'''length = input("What is the length of the shape?")
+width = input("What is the width of the shape?")
+if length == width: 
+  print("It is a sqaure")
+else: print("It is not a sqaure") '''
 
 
 # 7. You have had a great year and are going to offer a bonus of 10% to any employee who has a service of over 3 years. 
 #   Ask the user to input their current salary and years of service and print out their salary and their bonus or "No bonus" if they are not receiving one.
 
+''' 
+ sal = int(input("Enter your current salary: "))
+ year = int(input("Enter your years of service: "))
 
+if year > 3:
+   print (str(sal + sal *0.1))
+ else: print ("No Bonus!") '''
 
 # 8. Take a whole number input, if it's positive, print out the number cubed, if it is a negative, print out half its value.
-
-
+'''
+ numero = int(input("Enter a number: "))
+ if numero >0 :
+ print(numero**3)
+ else: print(numero/2) '''
+  
 
 
 # <---------------------------------------------------------------------------------------------->
@@ -42,7 +74,9 @@
 # 1. Ask for the user's name, if they are called "Alice" print "Hello, Alice", if they are called "Bob", 
 #   print "You're not Bob! I'm Bob", else print "You must be Charlie".
 
-
+'''name = input("What is your name?")
+ if name == "Alice":
+print '''
 
 # 2. Ask the user to enter their age:
 #     1. If they are younger than 11, print "You're too young to go to this school"
@@ -50,21 +84,55 @@
 #     3. If they are over 16, print 'You're too old for school"
 #     4. If they are 0, print "You're not born yet!"
 
+''' age = int(input("Enter your age: "))
+
+if age <= 11: 
+ print("You're too young to go to this school!")
+  
+ elif age >= 11 & age <= 16:
+   print("You can come to this school")
+  
+ elif age == 0:
+  print("You're not born yet!") '''
 
 
 # 3. Ask the user to enter the name of a month. If the user enters March/April/May: print "<month> is in Spring", otherwise print "I don't know".
 #     1. Expand for the rest of the year, given that summer is June/July/August. Autumn is September/October/November. Winter is December/January/February.
 #     2. Ensure that when an unknown month is given it prints "I don't know".
 
-
+''' month = input("What is the name of the month: ")
+if month == "March" or month == "April" or month == "May":
+  print(month+ " is in Spring! ")
+  
+elif month == "June" or month == "July" or month == "August":
+  print(month + " is in Summer!")
+elif month == "September" or month == "October":
+  print(month+ " is Winter!")
+else:
+    print("I don't know") 
+'''
 
 # 4. Ask the user for two different numbers, if both numbers are even, print "Even", if both numbers are odd, print "Odd", else print the product of the two numbers.
 
+'''number1 = input("Enter a number: ")
+number2 = input("Enter a number: ")
 
+if (number1 % 2 == 0 and number2 % 2 == 0):
+    print("Both numbers are even.")
+elif (number1 % 2 == 1 and number2 % 2 == 1):
+    print("Both numbers are odd.")
+else:
+    print(number1 * number2)'''
 
 # 5. Ask the user to input two numbers. Decide which is the number of highest value and print this out.
 
+'''number_val = int(input("Enter a number: "))
+number_val2 = int(input("Enter a number: "))
 
+if number_val > number_val2:
+  print(str(number_val) + " has the highest value")
+else: 
+  print(str(number_val2) + " has the highest value")'''
 
 # 6. You have had a fantastic year and are now going to offer a bonus of 20% to any employee who has a service of over 7 years, 
 #   a bonus of 15% to any employee who has a service of over 5 years and a bonus of 10% to any employee who has a service of 3 - 5 years. 
--- session_04/exercises_4.py
+++ session_04/exercises_4.py
@@ -3,31 +3,35 @@
 ## Section A
 # 1. Create the following list of items: Apples, Cherries, Pears, Pineapple, Peaches, Mango. Then print the list.
 
-
+fruits = ['Apples', 'Cherries', 'Pears', 'Pineabpple', 'Peaches', 'Mango']
+print(fruits)
 
 # 2. Add "Grapes" to the list and print the list.
 
-
+fruits.append('Grapes')
+print(fruits)
 
 # 3. Change "Pears" to "Strawberries" and print the list.
 
-
+fruits[2] = 'Strawberries'
+print(fruits)
 
 # 4. Remove "Apples" from the list and print the list.
 
-
+del(fruits[0])
+print(fruits)
 
 # 5. Print out the current length of the list.
 
-
+print(len(fruits))
 
 # 6. Order the list alphabetically.
 
-
+fruits.sort()
 
 # 7. Print out the list again.
 
-
+print(fruits)
 
 
 # <---------------------------------------------------------------------------------------------->
@@ -35,39 +39,83 @@
 ## Section B
 # 1. Loop through the list you created in section A and print each item out.
 
-
+for items in fruits:
+  print(fruits)
 
 # 2. Print the numbers 1 to 100 (including the number 100).
 
-
+'''for items in range (1,101):
+  print(items) '''
 
 # 3. Print all odd numbers from 1 to 100.
-
-
+for i in range(1, 101):
+    if i % 2:
+        print(i)
 
 # 4. The modern olympics started in 1896, print the years they have been held (bonus points to skip the years it has not been held 1916, 1940, 1944, 2020).
 
+for olympic_year in range(1896, 2022, 4):
+    print(olympic_year)
 
+# Bonus: skip the years it has not been 1916, 1940, 1944, 2020
+
+not_held = [1916, 1940, 1944, 2020]
+
+for olympic_year in range(1896, 2022, 4):
+    if olympic_year not in not_held:
+        print(olympic_year)
 
 # 5. Create a list of ten random numbers. Loop through your list and count the number of even numbers and the number of odd numbers.
 
+numbers = [1, 10, 13 , 15, 765, 32, 65, 23, 56, 101]
+even_count = 0
+odd_count = 0
 
+for i in numbers:
+    if i % 2 == 0:
+        even_count = even_count + 1
+    else:
+        # This is short hand for the line above
+        odd_count += 1
+
+print("This list has " + str(even_count) + " even numbers and " + str(odd_count) + " odd numbers.")
 
 # 6. Create a list of five names. Write a loop that will print "Hello" plus each name in the list.
-
+names = ['Stacey', 'Monday', 'Gina', 'Bob', 'Tracy']
+for items in names:
+  print ("Hello, " + items)
 
 
 # 7. Create a loop to go through each letter of the word "supercalifragilisticexpialidocious".
 
+word = "supercalifragilisticexpialidocious"
+
+for i in word:
+    print(i)
 
 
 # 8. Create a list of 5 numbers. Write a for loop which appends the square of each number to the new list.
+numbers = [2, 5, 8, 9, 10]
+sqr_numbers = []
 
+for i in numbers:
+    sqr_numbers.append(i ** 2)
+
+print(sqr_numbers)
 
 
 # 9. Create a list with five names in. Write a for loop which appends  Dr. to each name in the new list.
 
 
+namess = ['Stacey', 'London', 'Zaida', 'Honey', 'Bella']
+for items in namess:
+  print ("Dr " + items)
+
+names = ["Saf", "Graham", "Jake", "Sanj", "Fis"]
+drs = []
+for name in names:
+    drs.append("Dr. " + name)
+print(drs)
 
 # 10. FizzBuzz – Write a program that prints the numbers from 1 to 100. For multiples of three, print "Fizz" instead of the number and for multiples of five, print "Buzz". 
 #    For numbers which are multiples of both three and five, print "FizzBuzz".
@@ -89,3 +137,16 @@
 #     14
 #     FizzBuzz
 #     ```
+
+
+
+for number in range(1, 101):
+    if (number % 3 == 0) and (number % 5 == 0):
+        print("FizzBuzz")
+    elif (number % 3 == 0):
+        print("Fizz")
+    elif (number % 5 == 0):
+        print("Buzz")
+    else:
+        print(number)
+  
